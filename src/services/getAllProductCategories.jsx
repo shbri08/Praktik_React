0d1439a84de63392d3b0af6c67116918d0468e3b
shabrinazahra <shabrinazahra08@gmail.com>
2024-11-18 14:02:29 +0700

    first commit

---
+++ src/services/getAllProductCategories.jsx
@@ -0,0 +1,15 @@
+const getAllProductCategories = ()  => {
+    return [   {
+      id: 'baju',
+      slug: 'baju',
+      name: `Baju`
+    },
+    {
+      id: 'celana',
+      slug: 'celana',
+      name: `Celana`
+    },
+    ]
+  }
+  
+export default getAllProductCategories
---
+++ src/services/getAllProducts.js
@@ -0,0 +1,93 @@
+export default function getAllProducts() {
+  return [
+    {
+      id: 'ZR-001',
+      slug: 'Kemeja Crop Popelin',
+      name: "Kemeja Crop Popelin",
+      category: 'Baju',
+      categorySlug : 'baju',
+      price: 700_000,
+      stock: 100,
+      imageUrl: '/assets/images/Gambar baju 1.jpg',
+      description: `Kemeja crop kerah berdiri. Lengan pendek lebar. Penutup depan jenis polo dengan ritsleting.`
+    },
+    {
+      id: 'ZR-002',
+      slug: 'Kemeja Crop Kotak - Kotak',
+      name: "Kemeja Crop Kotak - Kotak",
+      category: 'Baju',
+      categorySlug : 'baju',
+      price: 599_900,
+      stock: 50,
+      imageUrl: '/assets/images/Gambar baju 2.jpg',
+      description: `Kemeja kerah lapel dan lengan panjang selesaian manset dengan lipit. Penutup depan dengan kancing. Penutup di belakang dengan ritsleting tertutup kampuh.`
+    },
+    {
+      id: 'ZR-003',
+      slug: 'Kemeja Katun Saku',
+      name: "Kemeja Katun Saku",
+      category: 'Baju',
+      categorySlug : 'baju',
+      price: 629_900,
+      stock: 5,
+      imageUrl: `/assets/images/Gambar baju 3.jpg`,
+      description: 'Kemeja terbuat dari katun 100%. Kerah lapel dan lengan panjang keliman manset. Saku tempel di bagian depan. Bagian bawah'
+    },
+    {
+      id: 'ZR-004',
+      slug: 'Blus Kerah Rebah',
+      name: "Blus Kerah Rebah",
+      category: 'Baju',
+      categorySlug : 'baju',
+      price: 799_900,
+      stock: 5,
+      imageUrl: `/assets/images/Gambar baju 4.jpg`,
+      description: 'Blus terbuat dari pintalan katun 100%. Kerah rebah dan lengan panjang selesaian manset. Bagian bawah bundar asimetris. Penutup depan dengan kancing dan simpul.'
+    },
+    {
+      id: 'ZR-005',
+      slug: 'Celana Panjang Bahan Jatuh Lipatan',
+      name: "Celana Panjang Bahan Jatuh Lipatan",
+      category: 'Celana',
+      categorySlug : 'celana',
+      price: 899_900,
+      stock: 6,
+      imageUrl: `/assets/images/Gambar Celana 1.jpg`,
+      description: `Celana panjang dibuat dengan 24% linen high waist dengan sengkelit sabuk. Saku samping dan saku paspoil hias di belakang. Detail lipit di depan. Penutup depan dengan ritsleting, kancing bagian dalam, dan kait logam.`
+    },
+    {
+      id: 'ZR-006',
+      slug: 'Celana Panjang Kupnat',
+      name: "Celana Panajang Kupnat",
+      category: 'Celana',
+      categorySlug : 'celana',
+      price: 699_900,
+      stock: 60,
+      imageUrl: `/assets/images/Gambar Celana 2.jpg`,
+      description: `Celana panjang high waist dengan kupnat depan. Saku depan dan saku paspoil hias di belakang. Penutup depan dengan ritsleting, kancing bagian dalam, dan kait.`
+    },
+    {
+      id: 'ZR-007',
+      slug: 'Celana Panjang Flare',
+      name: "Celana Panjang Flare",
+      category: 'Celana',
+      categorySlug : 'celana',
+      price: 999_900,
+      stock: 0,
+      imageUrl: `/assets/images/Gambar Celana 3.jpg`,
+      description: `Celana panjang high waist dan pinggang dengan sengkelit sabuk. Saku di depan dan di belakang. Detail garis setrika di depan. Kaki lurus. Bagian bawah selesaian bentuk A. Penutup depan dengan ritsleting, kancing, dan kait logam.`
+    },
+    {
+      id: 'ZR-008',
+      slug: 'Celana Panjang Satin',
+      name: "Celana Panjang Satin",
+      category: 'Celana',
+      categorySlug : 'celana',
+      price: 899_900,
+      stock: 55,
+      imageUrl: `/assets/images/Gambar Celana 4.jpg`,
+      description: `Celana panjang high waist dengan pinggang elastis. Kantong samping tertutup kampuh. Kaki lebar.`
+    },
+
+  ]
+}
